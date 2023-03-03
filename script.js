65eb203dd64160af281b4e3724293a4a2025858b
P-WRITES <126659202+P-WRITES@users.noreply.github.com>
2023-03-04 02:25:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+  const activePage = window.location.pathname;
+
+  const navLinks = document.querySelectorAll('nav a').
+  forEach(link => {
+    if(link.href.includes(`${activePage}`)) {
+      link.classList.add('active');
+    }
+  })
+
+
